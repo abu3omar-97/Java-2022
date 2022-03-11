0caa69cd73bc03be15608fabf3e094646ef72a41
Yaman <yalashqar97@gmail.com>
2022-03-11 21:46:21 +0300

    Week2

---
+++ Week1/CODE/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("PLEASE ENTER UR AGE:");
+//        int age = input.nextInt();
+//        System.out.println("Age+5:" + (age+5));
+
+//        char myFavChar = 'a';
+//        System.out.println((char)(myFavChar-32));
+
+
+//        boolean isLightOn = true;
+//
+//        isLightOn = !isLightOn;
+//        isLightOn = !isLightOn;
+//        isLightOn = !isLightOn;
+//
+//        System.out.println(isLightOn);
+
+//        int age = 28;
+//        boolean isAgeInTwenties = age >= 20 && age < 30; // 30 > age >= 20
+
+
+    }
+}
